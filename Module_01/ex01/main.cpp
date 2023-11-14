223c3c209deeaf24e9c10bde1e6c91fe7dce036f
Awena Salic <asalic@student.42.fr>
2023-11-14 14:01:46 +0100

    Module1, ex01 done

--- Module_01/ex01/main.cpp
+++ Module_01/ex01/main.cpp
@@ -3,8 +3,11 @@
 int    main(void)
 {
     Zombie  *zombies;
+    int     nb=3;
 
-    zombies = zombieHorde(15, "BenJ");
+    zombies = zombieHorde(nb, "BenJ");
+    for (int i=0; i < nb; i++)
+		zombies->announce();
     delete [] zombies;
     return (0);
 }
