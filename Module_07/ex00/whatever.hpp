ce333c3b01ce744ccaebb07d2d436b1b78a1c969
Awena Salic <awenasalic@gmail.com>
2024-02-23 17:12:20 +0100

    cpp07 a peine commencer

---
+++ Module_07/ex00/whatever.hpp
@@ -0,0 +1,12 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+# include <string>
+
+template <typename T>
+void    swap(T value1, T value2);
+T       min(T value1, T value2);
+T       max(T value1, T value2);
+
+#endif
