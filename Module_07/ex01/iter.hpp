a251e5e48fd59af2a130c1ba88032dba8bed0d3f
Awena Salic <awenasalic@gmail.com>
2024-02-27 14:22:10 +0100

    ex01 done

--- Module_07/ex01/iter.hpp
+++ Module_07/ex01/iter.hpp
@@ -1,17 +1,34 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+# include <iostream>
+
+# define NC "\033[0m"
+# define RED "\033[31;1m"
+# define GREEN "\033[32;1m"
+# define YELLOW "\033[33;1m"
+# define BLUE "\033[34;1m"
+# define MAGENTA "\033[35;1m"
+# define CYAN "\033[36;1m"
+# define WHITE "\033[37;1m"
+
 template <typename T>
-void    increment(T* array)
+void    print_array(T& array)
 {
-    array += 1;
+    std::cout << array << " ";
 };
 
 template <typename T>
-void    iter(T& addr, int len, void (*func)(T*))
+void    increment(T& array)
+{
+    array ++;
+};
+
+template <typename T>
+void    iter(T* addr, int len, void (*func)(T&))
 {
     for (int i=0; i < len; i ++){
-        (func)(*addr[i]);}
+        (func)(addr[i]);}
 };
 
 #endif
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -2,7 +2,25 @@
 
 int main(void)
 {
-    char array[3] = "he";
+    int     int_array[5] = {1, 2, 4, 7, 8};
+    char    char_array[18] = "Je suis a 42Paris";
+    float   float_array[6] = {42.0f, 42.2f, 42.99f, 4.2f, 0.42f, 8.10f};
 
-    ::iter(array, 3, ::increment);
+    std::cout << YELLOW"---[ Iter with print on a char array ]---" NC << std::endl;
+    ::iter(char_array, 18, ::print_array);
+    std::cout << "\n" << std::endl;
+
+    std::cout << YELLOW"---[ Iter with print/increment/print on an int array ]---" NC << std::endl;
+    ::iter(int_array, 5, ::print_array);
+    std::cout << std::endl;
+    ::iter(int_array, 5, ::increment);
+    ::iter(int_array, 5, ::print_array);
+    std::cout << "\n" << std::endl;
+
+    std::cout << YELLOW"---[ Iter with print/increment/print on a float array ]---" NC << std::endl;
+    ::iter(float_array, 6, ::print_array);
+    std::cout << "\n" << std::endl;
+    ::iter(float_array, 6, ::increment);
+    ::iter(float_array, 6, ::print_array);
+    std::cout << "\n" << std::endl;
 }
